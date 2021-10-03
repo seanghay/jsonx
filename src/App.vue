e51295c17ee134ad32a634f2f3330f0e165327bd
Yath Seanghay <seanghay.dev@gmail.com>
2021-10-03 18:24:04 +0700

    add github link

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <json-editor></json-editor>
+    <a class="m-4 text-sm text-gray-400" href="https://github.com/seanghay/jsonx" target="_blank">View on GitHub</a>
   </div>
+
 </template>
 <script>
 import JSONEditor from "./components/JSONEditor.vue";
